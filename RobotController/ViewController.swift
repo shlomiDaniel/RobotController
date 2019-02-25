37c93a9de60d371a7f6602eb7bb45812450d8f65
shlomi daniel <shlomi.daniel@gmail.com>
2019-02-26 01:06:39 +0200

    first

--- RobotController/ViewController.swift
+++ RobotController/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("heloo")
+        print("heloo")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
